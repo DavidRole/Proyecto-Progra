715a2031d2a63b96bb10a2bc84df991a604a5da8
CrisFer2301 <cristianfm2005@gmail.com>
2024-06-04 15:31:03 -0600

    Actualizacion ventana

--- src/Usuario/UserBuilder.java
+++ src/Usuario/UserBuilder.java
@@ -12,10 +12,10 @@ import java.util.GregorianCalendar;
  *
  * @author emanu
  */
-public class UserBuilder implements AbstractBuilder{
+public class UserBuilder implements AbstractBuilder {
 
     private User usuario;
-    
+
     @Override
     public void createUser() {
         usuario = new User();
@@ -23,95 +23,101 @@ public class UserBuilder implements AbstractBuilder{
 
     @Override
     public void createId(String id) {
-        if(id != null){
-            if(id.length() == 10 && id.matches("0\\d{9}")){
+        if (id != null) {
+            if (id.length() == 10 && id.matches("0\\d{9}")) {
                 usuario.setId(id);
             }
         }
     }
+
     @Override
     public void createEmail(String email) {
-        if(email != null){
-            if(email.matches("^[a-zA-Z][a-zA-Z0-9._]*@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")){
+        if (email != null) {
+            if (email.matches("^[a-zA-Z][a-zA-Z0-9._]*@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")) {
                 usuario.setEmail(email);
             }
         }
     }
+
     @Override
     public void createPassword(String password) {
-        String index = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?!.*(012|123|234|345|456|567|678|789|890|901))(?=.*[!@#$%&/()=+?\\[\\]~\\-^])[A-Za-z0-9!@#$%&/()=+?\\[\\]~\\-^]{10,16}$";
-        if(password.matches(index)){
-            usuario.setPassword(password);
+        if (password != null) {
+            String index = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?!.*(012|123|234|345|456|567|678|789|890|901))(?=.*[!@#$%&/()=+?\\[\\]~\\-^])[A-Za-z0-9!@#$%&/()=+?\\[\\]~\\-^]{10,16}$";
+            if (password.matches(index)) {
+                usuario.setPassword(password);
+            }
         }
+
     }
+
     @Override
     public void createBirthdayDate(GregorianCalendar birthdayDate) {
-        GregorianCalendar today = new GregorianCalendar();
-        GregorianCalendar maxDate = new GregorianCalendar();
-        GregorianCalendar minDate = new GregorianCalendar(); 
-        
-        minDate.add(Calendar.YEAR, -18);
-        
-        maxDate.add(Calendar.YEAR, -100);      
-        if(birthdayDate.before(minDate) && birthdayDate.after(maxDate)){
-            usuario.setBirthdayDate(birthdayDate);
+        if (birthdayDate != null) {
+            GregorianCalendar today = new GregorianCalendar();
+            GregorianCalendar maxDate = new GregorianCalendar();
+            GregorianCalendar minDate = new GregorianCalendar();
+
+            minDate.add(Calendar.YEAR, -18);
+
+            maxDate.add(Calendar.YEAR, -100);
+            if (birthdayDate.before(minDate) && birthdayDate.after(maxDate)) {
+                usuario.setBirthdayDate(birthdayDate);
+            }
+
         }
     }
 
     @Override
     public void createName(String name) {
-        if(name != null && name.matches("[a-zA-Z ]{1,30}")){
+        if (name != null && name.matches("[a-zA-Z ]{1,30}")) {
             usuario.setName(name);
         }
     }
 
     @Override
     public void createLastName(String lastName) {
-        if(lastName != null && lastName.matches("[a-zA-Z ]{1,30}")){
+        if (lastName != null && lastName.matches("[a-zA-Z ]{1,30}")) {
             usuario.setLastName(lastName);
         }
     }
 
     @Override
     public void createPhoneNumber(String phoneNumber) {
-       if(phoneNumber != null && phoneNumber.matches("^\\d{4}-\\d{4}$")){
-           usuario.setPhoneNumber(phoneNumber);
-       }
+        if (phoneNumber != null && phoneNumber.matches("^\\d{4}-\\d{4}$")) {
+            usuario.setPhoneNumber(phoneNumber);
+        }
     }
 
     @Override
     public User getUser() throws UserException {
-       String text = "";
-        
-        if(usuario.getId() == null){
+        String text = "";
+
+        if (usuario.getId() == null || usuario.getId().equals("")) {
             text += "El id deberá tener el formato de cédula costarricense. En el campo de entrada deberá mostrar una hilera "
-            + "de ayuda para indicar el formato, el cual consiste en 10 dígitos iniciando con 0.\n";
+                    + "de ayuda para indicar el formato, el cual consiste en 10 dígitos iniciando con 0.\n";
         }
-        if(usuario.getEmail() == null){
+        if (usuario.getEmail() == null || usuario.getEmail().equals("")) {
             text += "El correo deberá comenzar con una letra y podrá tener o no más letras, números, guiones bajo o puntos, "
                     + "luego debe estar el @ seguido de una o más letras, números, puntos o guiones cortos, seguidas de un punto y por último entre 2 y 4 letras.\n";
         }
-        if(usuario.getPassword() == null){
-            text += "La contraseña debe tener al menos una letra mayúscula (A – Z), tener al menos una letra minúscula (a – z), incluir números (0 - 9), "
+        if (usuario.getPassword() == null || usuario.getPassword().equals("")) {
+            text += "La contraseña debe tener al menos una letra mayúscula (A – Z), tener al menos una letra minúscula (a – z),\n" + " incluir números (0 - 9), "
                     + "los cuales no se pueden repetir de manera secuencial, tener 1 o más de los siguientes "
-                    + "caracteres especiales: ! @ # $ % & / ( ) = + ? [ ] ~ – ^, no se permite el ingreso de otros caracteres, "
-                    + "el campo de entrada debe ser enmascarado para que no se muestren los caracteres ingresados. \n";
+                    + "caracteres especiales: ! @ # $ % & / ( ) = + ? [ ] ~ – ^, no se permite el ingreso de otros caracteres.\n";
         }
-        if(usuario.getBirthdayDate() == null){
-            text += "La fecha de nacimiento deberá ser a una fecha válida y corresponder a una persona mayor de edad. "
-                    + "En el campo de entrada deberá mostrar una hilera de ayuda para indicar el formato, a menos que la fecha sea seleccionable.\n";
+        if (usuario.getBirthdayDate() == null) {
+            text += "La fecha de nacimiento deberá ser a una fecha válida y corresponder a una persona mayor de edad. ";
         }
-        if(usuario.getName() == null){
+        if (usuario.getName() == null || usuario.getName().equals("")) {
             text += "El nombre no debe contener números ni caracteres especiales y su largo no debe exceder los 30 caracteres.\n";
         }
-        if(usuario.getLastName() == null){
+        if (usuario.getLastName() == null||usuario.getLastName().equals("") ) {
             text += "El apellido no debe contener números ni caracteres especiales y su largo no debe exceder los 30 caracteres.\n";
         }
-        if(usuario.getPhoneNumber() == null){
-            text += "El numero telefonico deberá tener el formato de un teléfono costarricense. "
-                    + "En elcampo de entrada deberá mostrar una hilera de ayuda para indicar el formato\n";
+        if (usuario.getPhoneNumber() == null || usuario.getPhoneNumber().equals("")) {
+            text += "El numero telefonico deberá tener el formato de un teléfono costarricense. ";
         }
-        if(!text.equals(null)){
+        if (!text.equals("")) {
             throw new UserException(text);
         }
         return usuario;
--- src/View/FormRegister.java
+++ src/View/FormRegister.java
@@ -144,8 +144,11 @@ public class FormRegister extends javax.swing.JFrame {
         String lastName = jtxf_LastName.getText();
         String phoneNumber = jtxf_phoneNumber.getText();
         
-        JOptionPane.showMessageDialog(null, controler.Register(id, email, password, birthdaydate, name, lastName, phoneNumber));
-        dispose();
+        String menssage = controler.Register(id, email, password, birthdaydate, name, lastName, phoneNumber);
+        JOptionPane.showMessageDialog(null, menssage);
+        if (menssage.equals("Usuario registrado correctamente")) {
+            dispose();
+        }
     }//GEN-LAST:event_jbt_RegisterActionPerformed
 
     private void jbt_CancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbt_CancelActionPerformed
@@ -155,38 +158,6 @@ public class FormRegister extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FormRegister.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FormRegister.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FormRegister.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FormRegister.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new FormRegister().setVisible(true);
-            }
-        });
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField Jtxf_ID;
     private javax.swing.JLabel jLabel1;
